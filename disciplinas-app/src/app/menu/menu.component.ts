f13f40f8cac425f6455720b17a31a3049d686177
Fernando Casare <fercasare@gmail.com>
2023-10-27 00:01:37 -0300

    menu responsivo - abrindo e fechando

--- disciplinas-app/src/app/menu/menu.component.ts
+++ disciplinas-app/src/app/menu/menu.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import 'materialize-css';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.less']
 })
-export class MenuComponent {
+export class MenuComponent implements AfterViewInit {
 
+  constructor() {}
+
+  ngAfterViewInit() {
+    const elem = document.querySelector('.sidenav');
+    if (elem) {
+      const options = {};
+      M.Sidenav.init(elem, options);
+    }
+  }
+
+  // Função para abrir e fechar o menu mobile (sidenav)
+  toggleMobileMenu() {
+    const sidenav = document.querySelector('.sidenav');
+    const sidenavInstance = M.Sidenav.getInstance(sidenav!);
+
+    if (sidenavInstance) {
+      sidenavInstance.isOpen ? sidenavInstance.close() : sidenavInstance.open();
+    }
+  }
+
+  // Função para fechar o menu mobile (sidenav)
+  closeMobileMenu() {
+    const sidenav = document.querySelector('.sidenav');
+    const sidenavInstance = M.Sidenav.getInstance(sidenav!);
+
+    if (sidenavInstance) {
+      sidenavInstance.close();
+    }
+  }
 }
